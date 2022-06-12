0a3350938ea5c22469d291730f2d736316b41dcd
Santino <101665218+Santinou1@users.noreply.github.com>
2022-06-12 10:01:44 -0300

    Add files via upload

---
+++ MovieCard.jsx
@@ -0,0 +1,21 @@
+import styles from "./MovieCard.module.css";
+import { Link } from "react-router-dom";
+import { getMoviePost } from "../hooks/getMoviePost";
+export function MovieCard({ movie }) {
+  const imgUrl = getMoviePost(movie.poster_path, 300);
+
+  return (
+    <li className={styles.movieCard}>
+      <Link to={"/movies/" + movie.id}>
+        <img
+          width={230}
+          height={345}
+          className={styles.movieImage}
+          src={imgUrl}
+          alt={movie.title}
+        />
+        <div>{movie.title}</div>
+      </Link>
+    </li>
+  );
+}
