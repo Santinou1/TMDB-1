2a1dbe3d422d3765471bae301ffb2d62df05a450
Santino <101665218+Santinou1@users.noreply.github.com>
2022-06-12 22:53:58 -0300

    Search notebook

---
+++ components/Search.jsx
@@ -0,0 +1,29 @@
+import styles from "./Search.module.css";
+import { useHistory } from "react-router-dom";
+import { useQuery } from "../hooks/useQuery";
+
+export function Search() {
+  const query = useQuery();
+  const search = query.get("search");
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={`${styles.box}`} onSubmit={handleSubmit}>
+      <form name="search">
+        <input
+          type="text"
+          value={search ?? ""}
+          onChange={(e) => {
+            const value = e.target.value;
+            history.push("/?search=" + value);
+          }}
+          className={`${styles.input}`}
+          name="src"
+        />
+      </form>
+    </div>
+  );
+}
